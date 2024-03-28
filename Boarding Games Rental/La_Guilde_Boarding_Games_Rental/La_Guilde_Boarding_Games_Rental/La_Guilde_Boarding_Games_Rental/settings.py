61c0cb25658195977151f49a6793b5caf121214d
Florian De Boni <florian.de-boni@student-cs.fr>
2024-03-28 16:42:19 +0900

    Test de mise en prod

--- Boarding Games Rental/La_Guilde_Boarding_Games_Rental/La_Guilde_Boarding_Games_Rental/La_Guilde_Boarding_Games_Rental/settings.py
+++ Boarding Games Rental/La_Guilde_Boarding_Games_Rental/La_Guilde_Boarding_Games_Rental/La_Guilde_Boarding_Games_Rental/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +41,10 @@ INSTALLED_APPS = [
     'BGR',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,6 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
